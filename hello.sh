a9b709bdc9e0f02c83f1c92e20f5de144a260a03
glgalang <louieangelo.galang@gcash.com>
2022-02-02 10:53:24 +0800

    initial commit

---
+++ hello.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INPUT_STRING=hello
+while [ "$INPUT_STRING" != "bye" ]
+do
+echo "Please type something in (bye to quit)"
+read INPUT_STRING
+echo "You typed: $INPUT_STRING"
+done
+
+
+#for i in 1 2 3 4 5
+#do
+#echo "Looping ... number $i"
+#done
+
+
+#if [ x = y ]; then 
+#	echo True
+#else
+#	echo False
+#fi
+
+#echo "Hello GCash Cadets!"
+#echo "Welcome to Shell Scripting!"
+
+
